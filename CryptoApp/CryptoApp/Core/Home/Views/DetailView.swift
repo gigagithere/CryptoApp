350dbc123e0c9fed653671451bf769db11735be1
gigagithere <par-gluten-0f@icloud.com>
2024-11-01 15:41:56 +0100

    Added navigation, improved top scrollbar

---
+++ CryptoApp/CryptoApp/Core/Home/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  CryptoApp
+//
+//  Created by Bartosz Mrugała on 01/11/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
